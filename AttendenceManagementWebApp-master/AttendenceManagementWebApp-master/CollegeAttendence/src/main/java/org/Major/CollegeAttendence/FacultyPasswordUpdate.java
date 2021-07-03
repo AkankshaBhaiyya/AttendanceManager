14a821f45c497245c3f65c706e3ce4211f0890ec
AkankshaBhaiyya <akankshabhaiyya0508@gmail.com>
2021-07-03 18:40:28 +0530

    commit1

---
+++ AttendenceManagementWebApp-master/AttendenceManagementWebApp-master/CollegeAttendence/src/main/java/org/Major/CollegeAttendence/FacultyPasswordUpdate.java
@@ -0,0 +1,46 @@
+package org.Major.CollegeAttendence;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FacultyPasswordUpdate
+ */
+public class FacultyPasswordUpdate extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out=response.getWriter();
+		String fid=request.getParameter("facultyId");
+		String pwd=request.getParameter("pwd");
+		try
+		{
+			 Connection con = DatabaseConnection.initializeDatabase(); 
+	            PreparedStatement st = con 
+	                   .prepareStatement("update faculty set Password=? where FacultyId=?"); 
+	            st.setString(1, pwd);
+	            st.setString(2, fid); 
+	            int u=st.executeUpdate();
+	            if(u>0)
+	            {
+	            	response.sendRedirect("Login.jsp");
+	            }
+	            else
+	            {
+	            	response.sendRedirect("changePassword.jsp");
+	            }
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
